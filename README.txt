dee2e8e5f7c00859ac37657bf5c3991faf5771e4
glassbeat <glassbeat@gmail.com>
2008-08-04 11:16:52 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+techventory2
+
+This is a TurboGears (http://www.turbogears.org) project. It can be
+started by running the start-techventory2.py script.
---
+++ techventory2/controllers.py
@@ -0,0 +1,46 @@
+import turbogears as tg
+from turbogears import controllers, expose, flash
+# from techventory2 import model
+from turbogears import identity, redirect
+from cherrypy import request, response
+# from techventory2 import json
+# import logging
+# log = logging.getLogger("techventory2.controllers")
+
+class Root(controllers.RootController):
+    @expose(template="techventory2.templates.welcome")
+    # @identity.require(identity.in_group("admin"))
+    def index(self):
+        import time
+        # log.debug("Happy TurboGears Controller Responding For Duty")
+        flash("Your application is now running")
+        return dict(now=time.ctime())
+
+    @expose(template="techventory2.templates.login")
+    def login(self, forward_url=None, previous_url=None, *args, **kw):
+
+        if not identity.current.anonymous and identity.was_login_attempted() \
+                and not identity.get_identity_errors():
+            redirect(tg.url(forward_url or previous_url or '/', kw))
+
+        forward_url = None
+        previous_url = request.path
+
+        if identity.was_login_attempted():
+            msg = _("The credentials you supplied were not correct or "
+                   "did not grant access to this resource.")
+        elif identity.get_identity_errors():
+            msg = _("You must provide your credentials before accessing "
+                   "this resource.")
+        else:
+            msg = _("Please log in.")
+            forward_url = request.headers.get("Referer", "/")
+
+        response.status = 403
+        return dict(message=msg, previous_url=previous_url, logging_in=True,
+            original_parameters=request.params, forward_url=forward_url)
+
+    @expose()
+    def logout(self):
+        identity.current.logout()
+        redirect("/")
